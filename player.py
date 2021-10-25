29e295ae89ab0bb365bbea3730a395e0ec851ee6
ithomps <ithomps31@gmail.com>
2021-10-24 22:47:13 -0400

    Add member variables to player

--- player.py
+++ player.py
@@ -1,5 +1,26 @@
+from card import Card
 
 class Player:
+    """
+    Holds the information and cards of a player
+    """
+    def __init__(self, name):
+        self.points = 0
+        self.cards = []
+        self.name = name
 
-    def __init__(self):
-        self.name = "rick"
+    def getPoints(self):
+        return self.points
+
+    def getCards(self):
+        return self.cards
+
+    def getName(self):
+        return self.name
+
+    def setCards(self, cards):
+        self.cards = cards
+
+    def displayCards(self):
+        for card in cards:
+            print(card)
