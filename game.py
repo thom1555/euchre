22bb6cf37b3a672128fbcdf3658ef77e45de8bd8
ithomps <ithomps31@gmail.com>
2021-10-24 22:52:36 -0400

    New host for game, add imports

---
+++ game.py
@@ -0,0 +1,7 @@
+from player import Player
+from card import Card
+from shuffle import Shuffle
+
+class Game:
+    def __init__(self):
+        pass
--- play.py
+++
@@ -1,6 +0,0 @@
-
-def main():
-    print("Oi")
-
-if __name__ == '__main__':
-    main()
